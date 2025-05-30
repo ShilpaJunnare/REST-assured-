73952be75d42442b5ff01d3bf93355c6e1f15048
HP <HP@DESKTOP-SSUFFAA>
2025-05-30 21:57:18 +0530

    Commit and push

---
+++ AssignmentRestAssured/src/test/java/tests/PutPatchDeleteExample.java
@@ -0,0 +1,71 @@
+package tests;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+import io.restassured.RestAssured;
+import io.restassured.RestAssured.*;
+import io.restassured.http.ContentType;
+
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PutPatchDeleteExample {
+	
+//	@Test
+	public void testPut() {
+		JSONObject request = new JSONObject();
+		
+		request.put("name", "Raghav");
+		request.put("job","teacher");
+		System.out.println(request.toJSONString());
+		baseURI ="https://reqres.in/api";
+		
+		given().
+		header("Content-Type","application/json").
+		contentType(ContentType.JSON).accept(ContentType.JSON).
+		body(request.toJSONString()).
+		when().
+		put("/users/2").
+		then().
+		statusCode(200).
+		log().all();
+		}
+
+//	@Test
+	public void testPatch() {
+		JSONObject request = new JSONObject();
+		
+		request.put("name", "Raghav");
+		request.put("job","teacher");
+		System.out.println(request.toJSONString());
+		baseURI ="https://reqres.in/api";
+		
+		given().
+		header("Content-Type","application/json").
+		contentType(ContentType.JSON).accept(ContentType.JSON).
+		body(request.toJSONString()).
+		when().
+		patch("/api/users/2").
+		then().
+		statusCode(200).
+		log().all();
+		}
+	
+	@Test
+	public void testDelete() {
+		JSONObject request = new JSONObject();
+		
+	
+		baseURI ="https://reqres.in/api";
+		
+		when().
+		patch("/api/users/2").
+		then().
+		statusCode(401).
+		log().all();
+		}
+}
