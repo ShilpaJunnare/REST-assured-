ac7d238ccc02027f26d08129133fbbe7e6927b21
HP <HP@DESKTOP-SSUFFAA>
2025-06-01 18:19:33 +0530

    commit first program

--- AssignmentRestAssured/src/test/java/tests/PutPatchDeleteExample.java
+++ AssignmentRestAssured/src/test/java/tests/PutPatchDeleteExample.java
@@ -17,13 +17,11 @@ public class PutPatchDeleteExample {
 	
 //	@Test
 	public void testPut() {
-		JSONObject request = new JSONObject();
-		
+		JSONObject request = new JSONObject();	
 		request.put("name", "Raghav");
 		request.put("job","teacher");
 		System.out.println(request.toJSONString());
-		baseURI ="https://reqres.in/api";
-		
+		baseURI ="https://reqres.in/api";		
 		given().
 		header("Content-Type","application/json").
 		contentType(ContentType.JSON).accept(ContentType.JSON).
@@ -38,12 +36,10 @@ public class PutPatchDeleteExample {
 //	@Test
 	public void testPatch() {
 		JSONObject request = new JSONObject();
-		
 		request.put("name", "Raghav");
 		request.put("job","teacher");
 		System.out.println(request.toJSONString());
-		baseURI ="https://reqres.in/api";
-		
+		baseURI ="https://reqres.in/api";	
 		given().
 		header("Content-Type","application/json").
 		contentType(ContentType.JSON).accept(ContentType.JSON).
@@ -55,13 +51,11 @@ public class PutPatchDeleteExample {
 		log().all();
 		}
 	
-	@Test
+	
+//	@Test
 	public void testDelete() {
 		JSONObject request = new JSONObject();
-		
-	
-		baseURI ="https://reqres.in/api";
-		
+		baseURI ="https://reqres.in/api";		
 		when().
 		patch("/api/users/2").
 		then().
